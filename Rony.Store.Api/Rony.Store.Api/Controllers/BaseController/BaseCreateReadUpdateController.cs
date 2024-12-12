bad7998c05467b12c2802363fbdbedc5028cc23a
Ronald Liboni <ronald.liboni@acad.pucrs.br>
2024-12-11 22:32:11 -0300

    FEAT create BaseCreateReadUpdateController

---
+++ Rony.Store.Api/Rony.Store.Api/Controllers/BaseController/BaseCreateReadUpdateController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rony.Store.Domain.Contracts.Services.BaseServices;
+
+namespace Rony.Store.Api.Controllers.BaseController;
+
+[Controller]
+public class BaseCreateReadUpdateController<Entity,Id,Form,DTO>(IBaseService<Entity, Id> baseService) : ControllerBase
+{
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> FindByIdAsync([FromRoute] Id id)
+    {
+        return Ok(await baseService.FindDTOByIdAsync<DTO>(id));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync([FromBody] Form formDTO)
+    {
+        await baseService.CreateAsync(formDTO);
+        return Created();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateByIdAsync([FromRoute] Id id, [FromBody] Form formDTO)
+    {
+        await baseService.UpdateByIdAsync(formDTO, id);
+        return NoContent();
+    }
+}
