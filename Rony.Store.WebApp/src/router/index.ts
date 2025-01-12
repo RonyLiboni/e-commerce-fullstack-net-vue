7de26e8248375f6c68f1d9526a554b5e2eb98d25
Ronald Liboni <ronald.liboni@acad.pucrs.br>
2025-01-12 15:54:25 -0300

    FEAT add guard in components

--- Rony.Store.WebApp/src/router/index.ts
+++ Rony.Store.WebApp/src/router/index.ts
@@ -1,6 +1,9 @@
+import { useAuthStore } from "@/stores/security/authStore";
 import CustomerSearchView from "@/views/CustomerSearchView/CustomerSearchView.vue";
+import LoginView from "@/views/Login/LoginView.vue";
 import ProductForm from "@/views/Product/ProductForm.vue";
 import ProductManagementView from "@/views/Product/ProductManagementView.vue";
+import UnauthorizedView from "@/views/Unauthorized/UnauthorizedView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
@@ -9,24 +12,52 @@ const router = createRouter({
     {
       path: "/",
       name: "Home",
-      component: CustomerSearchView,
+      component: CustomerSearchView
     },
     {
       path: "/products-management",
       name: "ProductManagementView",
       component: ProductManagementView,
+      meta: { requiresAuth: true, roles: ['Product-FindById','Product-UpdateById','Product-Create','Product-Find'] }
     },
     {
       path: "/products-management/create",
       name: "CreateProductForm",
       component: ProductForm,
+      meta: { requiresAuth: true, roles: ['Product-Create'] }
     },
     {
       path: "/products-management/edit/:id",
       name: "EditProductForm",
       component: ProductForm,
+      meta: { requiresAuth: true, roles: ['Product-UpdateById'] }
+    },
+    {
+      path: "/login",
+      name: "LoginView",
+      component: LoginView
+    },
+    {
+      path: '/unauthorized',
+      name: 'UnauthorizedView',
+      component: UnauthorizedView
     }
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  const requiresAuth = to.meta.requiresAuth;
+  const allowedRoles = to.meta.roles as string[];
+  const userRoles = authStore.roles;
+
+  if (requiresAuth && !authStore.isUserLoggedIn) {
+    next({ name: 'LoginView' });
+  } else if (requiresAuth && allowedRoles && (userRoles && !allowedRoles.some(role => userRoles!.includes(role)))){
+    next({ name: 'UnauthorizedView' });
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ Rony.Store.WebApp/src/views/Unauthorized/UnauthorizedView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Not authorized</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
